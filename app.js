4f9eec8b275f693fcbbc1e057c8ae0f15e4c25ab
franpic <franpic@live.it>
2018-10-02 11:18:20 +0200

    d

--- app.js
+++ app.js
@@ -319,7 +319,7 @@ async function handlePostback (senderPsid, receivedPostback) {
   if (payload === 'inizia') {
     varConsultazioni[senderPsid] = new Consultazione()
 
-    var debug = true
+    var debug = false
     if (debug === true) {
       await varConsultazioni[senderPsid].setValoreInDato('PCCFNC88C20F262P')
       await varConsultazioni[senderPsid].setValoreInDato('1234567890123456')
