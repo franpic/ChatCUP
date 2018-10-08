57cbd49ba794e7b9b46e2c5757714c64dc16aba6
franpic <franpic@live.it>
2018-10-08 20:49:17 +0200

    corretta immagine

--- it/exprivia/healthcare/cup/classi/Dati/NumeroRicettaElettronica.js
+++ it/exprivia/healthcare/cup/classi/Dati/NumeroRicettaElettronica.js
@@ -8,7 +8,7 @@ class NumeroRicettaElettronica extends Dato {
       'Numero della Ricetta Elettronica',
       'il',
       'del',
-      'https://raw.githubusercontent.com/franpic/ChatCUP/master/Immagini/presentazioneDati/RicettaElettronicaNumIndicato.png',
+      'https://raw.githubusercontent.com/franpic/ChatCUP/master/Immagini/presentazioneDati/RicettaElettronicaNumIndicato2.png',
       /(?:\*|")(\w{5})(?:\n|.)*?(?:\*|")(\w{10})/g,
       /^160\w{2}\d{10}$/,
       'CCCCCCCCCCCCCCC'
